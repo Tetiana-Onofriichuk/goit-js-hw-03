db933ec9ed447fef91cfcdd879c8369f1d2a3d26
Tetiana <Korostijtatiana@gmail.com>
2025-04-05 19:02:56 +0300

    add files

--- js/task-3.js
+++ js/task-3.js
@@ -15,3 +15,23 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// другий варіант
+
+function filterArrayTwo(numbers, value) {
+  let allNumbers = [];
+
+  for (let number of numbers) {
+    if (number > value) {
+      allNumbers.push(number);
+    }
+  }
+
+  return allNumbers;
+}
+
+console.log(filterArrayTwo([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArrayTwo([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArrayTwo([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArrayTwo([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArrayTwo([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
